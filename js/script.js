669c0199141f24be2d91ac1baacb3e093fcf7a3f
emir <emir.azimov@gmail.com>
2021-05-19 15:00:48 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $('.slider-list').slick({
+    // centerMode: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+
+    // responsive: [
+    //   {
+    //     // breakpoint: 870,
+    //     // // settings: 'unslick',
+    //   },
+    //   // {
+    //   //   breakpoint: 800,
+    //   //   settings: {
+    //   //     slidesToShow: 1,
+    //   //     slidesToScroll: 1,
+    //   //   },
+    //   // },
+    // ],
+  })
+})
